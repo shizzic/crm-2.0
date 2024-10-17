5c399f24713fe4ff9c9bca924bba3abad42a46d7
shizzic <kotcich@mail.ru>
2024-10-17 23:10:47 +0300

    working on loadUserSettings func

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { watch } from 'vue'
 import { useUserStore, useSettingsStore } from '@stores'
+import { loadUserSettings } from '@stores/settings/saving'
 import { useRouter } from 'vue-router'
 
 const $router = useRouter()
@@ -8,7 +9,11 @@ const $settings = useSettingsStore()
 $settings.getLang()
 $settings.getTheme($settings.theme)
 
-watch(() => useUserStore().isLoggedIn, (value) => { $router.replace(value ? { name: "home" } : { name: "login" }) })
+loadUserSettings()
+watch(() => useUserStore().isLoggedIn, (value) => {
+  if (value) loadUserSettings()
+  $router.replace(value ? { name: "home" } : { name: "login" })
+})
 </script>
 
 <template>
--- src/stores/settings/saving.ts
+++ src/stores/settings/saving.ts
@@ -1,10 +1,18 @@
-import { getActivePinia } from 'pinia'
+import { getActivePinia, defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 import { useUserStore } from '@stores'
 import { fetcher } from '@composables/fetcher'
 import { setTimeout, clearTimeout } from 'worker-timers'
 
+interface UserSettingsResponse {
+  avatar: string
+  settings: {
+    [index: string]: any
+  }
+}
+
+const filename = 'settingsV2'
 const tiktak: Ref<number | null> = ref(null)
 
 export function saveUserSettings(): void {
@@ -17,17 +25,34 @@ export function saveUserSettings(): void {
 }
 
 function uploadSettings(): void {
-  // const storeDefinition = defineStore(storeName, activepinia.state.value[storeName])
-  // const store = storeDefinition(activepinia)
-  // store.$reset()
-
-  console.log(12)
   if (!useUserStore().isLoggedIn) return
-  const activepinia = getActivePinia()
-  if (!activepinia) return
   const request: any = {
     settings: JSON.parse(localStorage.getItem('settings') ?? JSON.stringify({})),
     sidebar: JSON.parse(localStorage.getItem('sidebar') ?? JSON.stringify({}))
   }
-  fetcher.put('user/user/set-user-settings?filename=settingsV2', request)
+  fetcher.put(`user/user/set-user-settings?filename=${filename}`, request)
+}
+
+export async function loadUserSettings(): Promise<void> {
+  if (!useUserStore().isLoggedIn) return
+  const activepinia = getActivePinia()
+  if (!activepinia) return
+
+  const r: UserSettingsResponse = await fetcher.get(
+    `user/user/get-user-settings?filename=${filename}`
+  )
+
+  for (const storeName in r.settings)
+    for (const fieldName in r.settings[storeName]) {
+      const field: any = r.settings[storeName][fieldName]
+      const storeDefinition = defineStore(storeName, activepinia.state.value[storeName])
+      const store = storeDefinition(activepinia)
+
+      if (!(fieldName in store.$state)) continue
+      store.$state[fieldName as keyof typeof store.$state] = field
+
+      //   store.$reset()
+    }
+
+  console.log(r)
 }
